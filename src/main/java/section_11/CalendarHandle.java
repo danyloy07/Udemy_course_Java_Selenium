9611c539ee9ef65801e165abc8089a444508b2a7
danyloyHP <iakimets.danil@gmail.com>
2025-07-16 19:43:59 +0200

    calendar

--- src/main/java/section_11/CalendarHandle.java
+++ src/main/java/section_11/CalendarHandle.java
@@ -18,28 +18,41 @@ public class CalendarHandle {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        String month = "9";
+        String date = "7";
+        String year = "2001";
+        String[] expectDate = {month,date,year};
+        driver.findElement(By.xpath("//button[@class='react-date-picker__calendar-button react-date-picker__button']")).click();
+        driver.findElement(By.xpath("//button[@class='react-calendar__navigation__label']")).click();
+        driver.findElement(By.xpath("//button[@class='react-calendar__navigation__label']")).click();
+        boolean found = false;
 
-        List<String> listOfValues= new ArrayList<>();
-        listOfValues.add("9");
-        listOfValues.add("7");
-        listOfValues.add("2027");
-        String in = "43";
-
-        driver.findElement(By.cssSelector(".react-date-picker__inputGroup")).click();
-        for (int i = 0; i < 2; i++){
-            driver.findElement(By.cssSelector(".react-calendar__navigation__label__labelText")).click();
+        while (!found) {
+            List<WebElement> handleYear = driver.findElements(By.xpath("//button[@class='react-calendar__tile react-calendar__decade-view__years__year']"));
+            for (int i = 0; i < handleYear.size(); i++) {
+                String ye = handleYear.get(i).getText().trim();
+                if (ye.equals(year)) {
+                    handleYear.get(i).click();
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                driver.findElement(By.xpath("//button[@class='react-calendar__navigation__arrow react-calendar__navigation__prev-button']"))
+                        .click();
+            }
         }
-        driver.findElement(By.xpath("//button[text()='"+listOfValues.get(2)+"']")).click();
-        driver.findElements(By.xpath("//button[contains(@class,'react-calendar__tile')]"))
-                .get(Integer.parseInt(listOfValues.get(0))-1).click();
-        driver.findElement(By.xpath("//abbr[text()='"+listOfValues.get(1)+"']")).click();
-
-        List<WebElement> actualList = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
-        for(int i =0; i<actualList.size();i++) {
-            System.out.println(actualList.get(i).getAttribute("value"));
-            Assert.assertEquals(actualList.get(i).getAttribute("value"), listOfValues.get(i));
+        Assert.assertEquals(driver.findElement(
+                        By.xpath("//span[@class='react-calendar__navigation__label__labelText react-calendar__navigation__label__labelText--from']"))
+                .getText().trim(), year);
+        driver.findElements(By.xpath("//abbr")).get(Integer.parseInt(month) - 1).click();
+        driver.findElement(By.xpath("//abbr[text()='" + date + "']")).click();
+       List<WebElement> actualList = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+        for (int i = 0; i < actualList.size(); i++) {
+            Assert.assertEquals(actualList.get(i).getAttribute("value"), expectDate[i]);
         }
-        driver.close();
+        driver.quit();
     }
 }
+
 
